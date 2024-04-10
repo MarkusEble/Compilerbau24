029c90f3374c845f16b403d413592d72edfdc2eb
ebauersandoval <elke-b-s@gmx.de>
2024-04-10 08:39:29 +0200

    feat: implemented suggestions

--- compiler/machines/DezimalMachine.java
+++ compiler/machines/DezimalMachine.java
@@ -6,41 +6,14 @@ public class DezimalMachine extends compiler.StateMachine{
     @Override
     public void initStateTable() {
         compiler.State startState =new compiler.State("start", false);
-        startState.addTransition('0',"start");
-        startState.addTransition('1',"start");
-        startState.addTransition('2',"start");
-        startState.addTransition('3',"start");
-        startState.addTransition('4',"start");
-        startState.addTransition('5',"start");
-        startState.addTransition('6',"start");
-        startState.addTransition('7',"start");
-        startState.addTransition('8',"start");
-        startState.addTransition('9',"start");
+        startState.addTransitionRange('0','9',"start");
         startState.addTransition('.',"expectedValueAfterPoint");
         addState(startState);
         compiler.State expectedValueAfterPointState = new compiler.State("expectedValueAfterPoint",false);
-        expectedValueAfterPointState.addTransition('0',"expectedMoreOrFinal");
-        expectedValueAfterPointState.addTransition('1',"expectedMoreOrFinal");
-        expectedValueAfterPointState.addTransition('2',"expectedMoreOrFinal");
-        expectedValueAfterPointState.addTransition('3',"expectedMoreOrFinal");
-        expectedValueAfterPointState.addTransition('4',"expectedMoreOrFinal");
-        expectedValueAfterPointState.addTransition('5',"expectedMoreOrFinal");
-        expectedValueAfterPointState.addTransition('6',"expectedMoreOrFinal");
-        expectedValueAfterPointState.addTransition('7',"expectedMoreOrFinal");
-        expectedValueAfterPointState.addTransition('8',"expectedMoreOrFinal");
-        expectedValueAfterPointState.addTransition('9',"expectedMoreOrFinal");
+        expectedValueAfterPointState.addTransitionRange('0','9',"expectedMoreOrFinal");
         addState(expectedValueAfterPointState);
         compiler.State expectedMoreOrFinalState = new compiler.State("expectedMoreOrFinal", true);
-        expectedValueAfterPointState.addTransition('0',"expectedMoreOrFinal");
-        expectedValueAfterPointState.addTransition('1',"expectedMoreOrFinal");
-        expectedValueAfterPointState.addTransition('2',"expectedMoreOrFinal");
-        expectedValueAfterPointState.addTransition('3',"expectedMoreOrFinal");
-        expectedValueAfterPointState.addTransition('4',"expectedMoreOrFinal");
-        expectedValueAfterPointState.addTransition('5',"expectedMoreOrFinal");
-        expectedValueAfterPointState.addTransition('6',"expectedMoreOrFinal");
-        expectedValueAfterPointState.addTransition('7',"expectedMoreOrFinal");
-        expectedValueAfterPointState.addTransition('8',"expectedMoreOrFinal");
-        expectedValueAfterPointState.addTransition('9',"expectedMoreOrFinal");
+        expectedValueAfterPointState.addTransitionRange('0','9',"expectedMoreOrFinal");
         addState(expectedMoreOrFinalState);
     }
 
@@ -51,6 +24,6 @@ public class DezimalMachine extends compiler.StateMachine{
 
     @Override
     public TokenIntf.Type getType() {
-        return compiler.TokenIntf.Type.EOF;
+        return TokenIntf.Type.DECIMAL;
     }
 }
