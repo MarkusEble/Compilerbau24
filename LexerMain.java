ffc1faa71a72140dcdc129ac75b065f575f02a8b
Eble <markus.eble@sap.com>
2024-04-11 11:40:31 +0200

    Lexer solution

--- LexerMain.java
+++ LexerMain.java
@@ -6,8 +6,8 @@ public class LexerMain {
         compiler.Lexer lexer = new compiler.Lexer();
         OutputStreamWriter outStream = new OutputStreamWriter(System.out, "UTF-8");
 
-        lexer.processInput("12 45 3.4", outStream);
-        lexer.processInput("1AB-2", outStream);
+        lexer.processInput("3.4 AB WHILE", outStream);
+        lexer.processInput("/* */ A 45", outStream);
     }
 
 }
