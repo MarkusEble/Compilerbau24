95c40223a167ce14329f217805a27bd569ebc4db
Eble <markus.eble@sap.com>
2024-04-18 12:31:22 +0200

    Expression PLUS|MINUS

--- ExpressionEvaluatorMain.java
+++ ExpressionEvaluatorMain.java
@@ -5,7 +5,7 @@ public class ExpressionEvaluatorMain {
     public static void main(String[] args) throws Exception {
         compiler.Lexer lexer = new compiler.Lexer();
         compiler.ExpressionEvaluator evaluator = new compiler.ExpressionEvaluator(lexer);
-        int result = evaluator.eval("42");
+        int result = evaluator.eval("42-5-17*2");
         System.out.println(result);
     }
 
