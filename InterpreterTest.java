49a3312356c596ccebb1b9f54cdf751a283d2473
Eble <markus.eble@sap.com>
2024-05-09 14:47:34 +0200

    Interpreter test

---
+++ InterpreterTest.java
@@ -0,0 +1,14 @@
+import java.io.OutputStreamWriter;
+
+public class InterpreterTest implements test.TestCaseIntf {
+
+    @Override
+    public void executeTest(String input, OutputStreamWriter outputWriter) throws Exception {
+        compiler.CompileEnv env = new compiler.CompileEnv(input, false);
+        env.compile();
+        env.execute(outputWriter);
+        outputWriter.write("\n");
+        outputWriter.flush();
+    }
+
+}
