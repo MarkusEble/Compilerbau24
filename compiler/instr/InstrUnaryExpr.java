dc2b41e167d5cf43fb8bad97ad3ba8b4e45a2b71
Manuel Brauneis <manuel.brauneis@sap.com>
2024-05-13 13:09:53 +0200

    minor changes

--- compiler/instr/InstrUnaryExpr.java
+++ compiler/instr/InstrUnaryExpr.java
@@ -5,12 +5,11 @@ import compiler.ExecutionEnvIntf;
 import java.io.OutputStreamWriter;
 
 public class InstrUnaryExpr extends compiler.InstrIntf {
-    private compiler.InstrIntf m_lhs;
-    private compiler.InstrIntf m_rhs;
+    private compiler.InstrIntf m_hs;
     private compiler.TokenIntf.Type m_op;
     public InstrUnaryExpr(compiler.TokenIntf.Type op, compiler.InstrIntf child) {
         m_op = op;
-        m_lhs = child;
+        m_hs = child;
     }
 
     @Override
