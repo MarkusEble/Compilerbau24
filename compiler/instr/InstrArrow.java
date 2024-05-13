a8b0e8b1ea4abc70da911f1e220c7f926e4fd007
lica200 <johanna@eitel.de>
2024-05-13 13:23:34 +0200

    Feat: Instr and codegen for ArrowExpr

---
+++ compiler/instr/InstrArrow.java
@@ -0,0 +1,33 @@
+package compiler.instr;
+
+import compiler.ExecutionEnvIntf;
+import compiler.TokenIntf;
+
+import java.io.OutputStreamWriter;
+
+public class InstrArrow extends compiler.InstrIntf{
+    private compiler.InstrIntf m_lhs;
+    private compiler.InstrIntf m_rhs;
+
+    public InstrArrow(compiler.InstrIntf lhs, compiler.InstrIntf rhs) {
+        m_lhs = lhs;
+        m_rhs = rhs;
+    }
+
+    @Override
+    public void execute(ExecutionEnvIntf env) throws Exception {
+        int lresult = m_lhs.getValue();
+        int rresult = m_rhs.getValue();
+        if (lresult>=rresult){
+            m_value = lresult-rresult;
+        }else{
+            m_value = rresult+lresult;
+        }
+    }
+
+    @Override
+    public void trace(OutputStreamWriter os) throws Exception {
+        os.write(TokenIntf.Type.ARROW.toString());
+        os.write("\n");
+    }
+}
