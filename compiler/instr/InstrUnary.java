135cff9adb13c25d405406b41181b82142a8fe26
Integraluminium <95375836+Integraluminium@users.noreply.github.com>
2024-05-16 00:11:50 +0200

    add codegen for UnaryExpr

---
+++ compiler/instr/InstrUnary.java
@@ -0,0 +1,31 @@
+package compiler.instr;
+
+import compiler.ExecutionEnvIntf;
+import compiler.TokenIntf;
+
+import java.io.OutputStreamWriter;
+
+public class InstrUnary extends compiler.InstrIntf{
+    private final compiler.TokenIntf.Type m_type;
+    private final compiler.InstrIntf m_child;
+    public InstrUnary(compiler.TokenIntf.Type type, compiler.InstrIntf child) {
+        this.m_child = child;
+        this.m_type = type;
+    }
+
+    @Override
+    public void execute(ExecutionEnvIntf env) throws Exception {
+        int result = m_child.getValue();
+        if(m_type == compiler.TokenIntf.Type.NOT){
+            m_value = result > 0 ? 0 : 1;
+        } else if (m_type == TokenIntf.Type.MINUS) {
+            m_value =   - result;
+        }
+    }
+
+    @Override
+    public void trace(OutputStreamWriter os) throws Exception {
+        os.write(m_type.toString());
+        os.write("\n");
+    }
+}
