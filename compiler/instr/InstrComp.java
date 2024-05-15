a85821b92201978c2f7ffc4f57487e79a5152cf0
Thomas <wall.thomas.a22@student.dhbw-karlsruhe.de>
2024-05-15 12:49:41 +0200

    Added codegen for comp node

---
+++ compiler/instr/InstrComp.java
@@ -0,0 +1,33 @@
+package compiler.instr;
+
+import compiler.ExecutionEnvIntf;
+import compiler.Token;
+
+import java.io.OutputStreamWriter;
+
+public class InstrComp extends compiler.InstrIntf{
+    private compiler.InstrIntf lhsInstr, rhsInstr;
+    private Token token;
+
+    public InstrComp(compiler.InstrIntf lhsInstr, compiler.InstrIntf rhsInstr, Token token){
+        this.lhsInstr = lhsInstr;
+        this.rhsInstr = rhsInstr;
+        this.token = token;
+    }
+
+    @Override
+    public void execute(ExecutionEnvIntf env) throws Exception {
+        if(token.m_type == Token.Type.GREATER){
+            m_value = lhsInstr.getValue() > rhsInstr.getValue() ? 1 : 0;
+        } else if(token.m_type == Token.Type.LESS){
+            m_value = lhsInstr.getValue() < rhsInstr.getValue() ? 1 : 0;
+        } else {
+            m_value = lhsInstr.getValue() == rhsInstr.getValue() ? 1 : 0;
+        }
+    }
+
+    @Override
+    public void trace(OutputStreamWriter os) throws Exception {
+        os.write(token.toString() + "\n");
+    }
+}
