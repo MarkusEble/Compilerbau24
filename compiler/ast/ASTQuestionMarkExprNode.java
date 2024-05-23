d9ec6e7b44bafdb2039b7695659c96ba66236a18
Integraluminium <95375836+Integraluminium@users.noreply.github.com>
2024-05-23 12:03:15 +0200

    removed optionals in question mark expressions
    Signed-off-by: Integraluminium <95375836+Integraluminium@users.noreply.github.com>

--- compiler/ast/ASTQuestionMarkExprNode.java
+++ compiler/ast/ASTQuestionMarkExprNode.java
@@ -9,63 +9,44 @@ import java.util.Optional;
 
 public class ASTQuestionMarkExprNode extends ASTExprNode {
     private final ASTExprNode m_condition;
-    private Optional<ASTExprNode> m_result1 = Optional.empty();
-    private Optional<ASTExprNode> m_result2 = Optional.empty();
+    private final ASTExprNode m_result1;
+    private final ASTExprNode m_result2;
 
     public ASTQuestionMarkExprNode(ASTExprNode condition, ASTExprNode result1, ASTExprNode result2) {
         m_condition = condition;
-        m_result1 = Optional.of(result1);
-        m_result2 = Optional.of(result2);
-    }
-
-    public ASTQuestionMarkExprNode(ASTExprNode condition) {
-        m_condition = condition;
+        m_result1 = result1;
+        m_result2 = result2;
     }
 
     @Override
     public int eval() {
-        if (m_result1.isPresent() && m_result2.isPresent()) {
-            return m_condition.eval() > 0 ? m_result1.get().eval() : m_result2.get().eval();
-        } else {
-            return m_condition.eval();
-        }
+        return m_condition.eval() > 0 ? m_result1.eval() : m_result2.eval();
     }
 
     @Override
     public void print(OutputStreamWriter outStream, String indent) throws Exception {
-        if (m_result1.isPresent() && m_result2.isPresent()) {
-            outStream.write(indent);
-            outStream.write("QUESTION_MARK_EXPR");
-            outStream.write("\n");
-            m_condition.print(outStream, indent + "  ");
-            outStream.write(indent + "  ");
-            outStream.write("QUESTION_MARK");
-            outStream.write("\n");
-            m_result1.get().print(outStream, indent + "  ");
-            outStream.write(indent + "  ");
-            outStream.write("DOUBLE_COLON");
-            outStream.write("\n");
-            m_result2.get().print(outStream, indent + "  ");
-        } else {
-            m_condition.print(outStream, indent);
-        }
-        outStream.flush();
+        outStream.write(indent);
+        outStream.write("QUESTION_MARK_EXPR");
+        outStream.write("\n");
+        m_condition.print(outStream, indent + "  ");
+        outStream.write(indent + "  ");
+        outStream.write("QUESTION_MARK");
+        outStream.write("\n");
+        m_result1.print(outStream, indent + "  ");
+        outStream.write(indent + "  ");
+        outStream.write("DOUBLE_COLON");
+        outStream.write("\n");
+        m_result2.print(outStream, indent + "  ");
     }
 
     @Override
-    public InstrIntf codegen(CompileEnvIntf compileEnvIntf){
-        Optional<InstrIntf> result1;
-        Optional<InstrIntf> result2;
-        if (m_result1.isPresent() && m_result2.isPresent()){
-            result1 = Optional.of(m_result1.get().codegen(compileEnvIntf));
-            result2 = Optional.of(m_result2.get().codegen(compileEnvIntf));
-        } else {
-            result1 = Optional.empty();
-            result2 = Optional.empty();
-        }
+    public InstrIntf codegen(CompileEnvIntf compileEnvIntf) {
+        InstrIntf result1 = m_result1.codegen(compileEnvIntf);
+        InstrIntf result2 = m_result2.codegen(compileEnvIntf);
+
         InstrIntf condition = m_condition.codegen(compileEnvIntf);
         InstrIntf questionMarkExpr = new InstrQuestionMarkExpr(result1, result2, condition);
         compileEnvIntf.addInstr(questionMarkExpr);
-        return  questionMarkExpr;
+        return questionMarkExpr;
     }
 }
--- compiler/instr/InstrQuestionMarkExpr.java
+++ compiler/instr/InstrQuestionMarkExpr.java
@@ -7,11 +7,11 @@ import compiler.TokenIntf;
 import java.io.OutputStreamWriter;
 import java.util.Optional;
 
-public class InstrQuestionMarkExpr extends InstrIntf{
-    private final Optional<InstrIntf> m_lhsInstruction, m_rhsInstruction;
+public class InstrQuestionMarkExpr extends InstrIntf {
+    private final InstrIntf m_lhsInstruction, m_rhsInstruction;
     private final InstrIntf m_condition;
 
-    public InstrQuestionMarkExpr(Optional<InstrIntf> lhsInstruction, Optional<InstrIntf> rhsInstruction, InstrIntf condition) {
+    public InstrQuestionMarkExpr(InstrIntf lhsInstruction, InstrIntf rhsInstruction, InstrIntf condition) {
         this.m_lhsInstruction = lhsInstruction;
         this.m_rhsInstruction = rhsInstruction;
         this.m_condition = condition;
@@ -19,15 +19,11 @@ public class InstrQuestionMarkExpr extends InstrIntf{
 
     @Override
     public void execute(ExecutionEnvIntf env) throws Exception {
-        if (m_lhsInstruction.isPresent() && m_rhsInstruction.isPresent()) {
-            m_value = m_condition.getValue() > 0 ? m_lhsInstruction.get().getValue() : m_rhsInstruction.get().getValue();
-        } else {
-            m_value = m_condition.getValue();
-        }
+        m_value = m_condition.getValue() > 0 ? m_lhsInstruction.getValue() : m_rhsInstruction.getValue();
     }
 
     @Override
     public void trace(OutputStreamWriter os) throws Exception {
-        os.write(String.format("%%%d = %%%d %s %%%d, %%%d\n", m_id, m_condition.getId(), TokenIntf.Type.QUESTIONMARK.toString(), m_lhsInstruction.get().getId(), m_rhsInstruction.get().getId()));     
+        os.write(String.format("%%%d = %%%d %s %%%d, %%%d\n", m_id, m_condition.getId(), TokenIntf.Type.QUESTIONMARK, m_lhsInstruction.getId(), m_rhsInstruction.getId()));
     }
 }
