ca921e44ed39aaecaf69eee99ee289ad008b5034
Eble <markus.eble@sap.com>
2024-05-23 11:52:06 +0200

    ConstFold skeleton

--- compiler/ast/ASTExprNode.java
+++ compiler/ast/ASTExprNode.java
@@ -3,4 +3,5 @@ package compiler.ast;
 public abstract class ASTExprNode extends ASTNode {
     public abstract int eval();
     public compiler.InstrIntf codegen(compiler.CompileEnvIntf env) { return null; }
+    public Integer consFold() { return null; }
 }
