773221f4d6421e78db9b19c923120fa4bbd9860a
Integraluminium <95375836+Integraluminium@users.noreply.github.com>
2024-05-28 19:24:00 +0200

    fixed function parameters are now read in correct order, compares amount of parameters
    Signed-off-by: Integraluminium <95375836+Integraluminium@users.noreply.github.com>

--- compiler/ast/ASTExprFunctionCallNode.java
+++ compiler/ast/ASTExprFunctionCallNode.java
@@ -6,7 +6,8 @@ import compiler.instr.InstrPopReturn;
 
 import java.io.OutputStreamWriter;
 import java.util.List;
-public class ASTExprFunctionCallNode extends ASTExprNode{
+
+public class ASTExprFunctionCallNode extends ASTExprNode {
     private final List<ASTExprNode> paramList;
     private final String functionName;
 
@@ -32,13 +33,30 @@ public class ASTExprFunctionCallNode extends ASTExprNode{
     @Override
     public InstrIntf codegen(CompileEnvIntf env) {
         // Generate parameters
-        List<InstrIntf>parameterValues = paramList.stream().map(node -> node.codegen(env)).toList();
+        List<InstrIntf> parameterValues = paramList.stream().map(node -> node.codegen(env)).toList();
 
         // Get instructions of function
-        compiler.InstrBlock functionBlock = env.getFunctionTable().getFunction(functionName).m_body;
+        compiler.FunctionInfo function = env.getFunctionTable().getFunction(functionName);
+        compiler.InstrBlock functionBlock = function.m_body;
+
+        // check parameters // no type check
+        List<String> requiredParameters = function.varNames;
+        if (requiredParameters.size() != parameterValues.size()) {
+
+            throw new Error(String.format("""
+                            Amount of provided Parameters differs!
+                            Calling the function %s[%s]
+                            provided %d, expected %d
+                            """,
+                    function.m_name,
+                    String.join(", ", requiredParameters),
+                    parameterValues.size(),
+                    requiredParameters.size()
+            ));
+        }
 
         // Create instructions for function and return call
-        InstrIntf functionInstr = new InstrFunctionCall(parameterValues,functionBlock);
+        InstrIntf functionInstr = new InstrFunctionCall(parameterValues, functionBlock);
         InstrIntf returnInstr = new InstrPopReturn();
         env.addInstr(functionInstr);
         env.addInstr(returnInstr);
--- compiler/instr/InstrFunctionLoadParameter.java
+++ compiler/instr/InstrFunctionLoadParameter.java
@@ -2,6 +2,7 @@ package compiler.instr;
 
 import compiler.ExecutionEnvIntf;
 import compiler.InstrIntf;
+import compiler.Symbol;
 import compiler.SymbolTableIntf;
 
 import java.io.OutputStreamWriter;
@@ -20,10 +21,11 @@ public class InstrFunctionLoadParameter extends InstrIntf {
     @Override
     public void execute(ExecutionEnvIntf env) throws Exception {
         // load parameter values into symbol table
-        paramList.forEach(param -> {
-            compiler.Symbol symbol = symbolTable.getSymbol(param);
+        for (int i = paramList.size()-1; i>=0; i--) { // reversed, because parameters are on stack
+            String param = paramList.get(i);
+            Symbol symbol = symbolTable.getSymbol(param);
             symbol.m_number = env.pop();
-        });
+        }
     }
 
     @Override
